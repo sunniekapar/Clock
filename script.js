7962086c1534c6928fb138f2a75249fed17cfd1e
Sunnie Kapar <107890031+sunniekapar@users.noreply.github.com>
2022-08-08 14:31:10 -0400

    includes time period

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+let date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
 let difference = 12;
+let period = hours > 12 ? "PM": "AM";
 let clock = () => {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    if(hours == 0) hours = 12;
+    date = new Date();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+    if (hours == 0) hours = 12;
     hours = hours > 12 ? hours - difference : hours;
-    hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-    let time = hours + ":" + minutes + ":" + seconds;
+    let time = hours + ":" + minutes + ":" + seconds + " " + period;
     document.getElementById("clock").innerHTML = time;
     setTimeout(clock, 1000);
 };
 clock();
 
 const slider = document.getElementById("checkbox");
-slider.addEventListener('change', function() {
-    difference = this.checked ? 0 : 12;
+slider.addEventListener('change', function () {
+    if(this.checked) {
+        difference = 0;
+        period = "";
+    }  else {
+        difference = 12;
+        period = hours > 12 ? "PM": "AM";
+    }
     clock();
 }); 
