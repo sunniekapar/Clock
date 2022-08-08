209bc31810f144a350d7031e7968e695031525e2
Sunnie Kapar <107890031+sunniekapar@users.noreply.github.com>
2022-08-08 13:49:12 -0400

    finished

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
+let difference = 12;
 let clock = () => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-
+    if(hours == 0) hours = 12;
+    hours = hours > 12 ? hours - difference : hours;
     hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -14,3 +15,9 @@ let clock = () => {
     setTimeout(clock, 1000);
 };
 clock();
+
+const slider = document.getElementById("checkbox");
+slider.addEventListener('change', function() {
+    difference = this.checked ? 0 : 12;
+    clock();
+}); 
